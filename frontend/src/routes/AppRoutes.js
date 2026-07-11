7fe85ea3b486f2d69d697efd9b67d47ee015ff3d
GHemaSree <hemasree.guddanti@gmail.com>
2026-07-11 21:54:58 +0530

    Progress Pages, Quiz, Score, frontend updated

--- frontend/src/routes/AppRoutes.js
+++ frontend/src/routes/AppRoutes.js
@@ -6,6 +6,9 @@ import Topics from "../pages/Topics/Topics";
 import Trail from "../pages/Trail/Trail";
 import Module from "../pages/Module/Module";
 import Notes from "../pages/Notes/Notes";
+import Quiz from "../pages/Quiz/Quiz";
+import Score from "../pages/Score/Score";
+import Progress from "../pages/Progress/Progress";
 import ProtectedRoute from "./ProtectedRoute";
 
 function AppRoutes() {
@@ -50,6 +53,30 @@ function AppRoutes() {
         element={
           <ProtectedRoute>
             <Notes />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/quiz/:moduleId"
+        element={
+          <ProtectedRoute>
+            <Quiz />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/score"
+        element={
+          <ProtectedRoute>
+            <Score />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/progress"
+        element={
+          <ProtectedRoute>
+            <Progress />
           </ProtectedRoute>
         }
       />
